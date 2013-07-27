e118270c98777b6728f438800d594af612990e3c
Python Newbie <pynewb@gmail.com>
2013-07-27 19:23:14 -0400

    Serve all files via node.js.  Add client DELETE support (but not server).

--- README.md
+++ README.md
@@ -2,3 +2,22 @@ knockout
 ========
 
 Learning to use knockout
+
+You need to download both knockout.js and jquery.
+
+The server is based on node.js with Express and
+the MongoDB client.
+
+node.js:
+nodejs.org: used v0.10.13
+Mac: /usr/local/bin is where node and npm are installed
+
+Express info:
+http://blog.modulus.io/nodejs-and-express-create-rest-api
+Specify via package.json, install using npm install
+
+MongoDB:
+sudo brew update
+sudo brew install mongodb
+or
+sudo brew upgrade mongodb
--- app.js
+++ app.js
@@ -1,24 +1,62 @@
 var express = require('express');
-var app = express();
+var mongo = require('mongodb');
 
+var app = express();
+app.use(express.logger());
+app.use(express.static(__dirname + "/public"));
 app.use(express.bodyParser());
 
-app.get('/shows', function(req, res) {
-    //res.type('text/plain'); // set content-type
-    //res.send('i am a beautiful butterfly'); // send text response
-    
-    console.log("get /shows");
-    
-    var people = [
-        { firstName: 'Bert', lastName: 'Bertington' },
-        { firstName: 'Charles', lastName: 'Charlesforth' },
-        { firstName: 'Denise', lastName: 'Dentiste' }        
-    ];
-
-    console.log("return " + JSON.stringify(people));
-    
-    res.jsonp(people);
+var server = new mongo.Server('localhost', 27017, {auto_reconnect: true});
+var mongoClient = new mongo.MongoClient(server);
+mongoClient.open(function(err, mongoClient) {
+  if (err) {
+    console.log('Error connecting to MongoDB: ' + err);
+  } else {
+    appDb = mongoClient.db("app");
+    console.log("Connected to 'app' database");
+    appDb.collection('people', {strict:true}, function (err, collection) {
+      if (err) {
+        console.log("The 'people' collection doesn't exist. Creating it with sample data...");
+        populateDB();
+      }
+    });
+  }
 });
+
+app.get('/people', function(req, res) {
+  //res.type('text/plain'); // set content-type
+  //res.send('i am a beautiful butterfly'); // send text response
+    
+  console.log("get /people");
+    
+  appDb.collection('people', function(err, collection) {
+    collection.find().toArray(function(err, people) {
+      console.log("return " + JSON.stringify(people));        
+      res.jsonp(people);
+    });
+  });
+});
+
+/*--------------------------------------------------------------------------------------------------------------------*/
+// Populate database with sample data -- Only used once: the first time the application is started.
+// You'd typically not find this code in a real-life app, since the database would already exist.
+var populateDB = function() {
+ 
+  var people = [
+    { firstName: 'Bert', lastName: 'Bertington' },
+    { firstName: 'Charles', lastName: 'Charlesforth' },
+    { firstName: 'Denise', lastName: 'Dentiste' }        
+  ];
+
+  appDb.collection('people', function(err, collection) {
+    if (err) {
+      console.log("Unable to connect to collection 'people' to populate it");
+    } else {
+      collection.insert(people, {safe:true}, function (err, result) {
+      });
+    }
+  }); 
+};
 
 var port = process.env.PORT || 4730
 app.listen(port);
